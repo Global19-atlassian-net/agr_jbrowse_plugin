d24f9bd6cd531dfd53aa3bb6458ed6ad28853337
Scott Cain <scott+git@scottcain.net>
2020-08-20 21:56:44 +0000

    improving the url building logic

--- js/main.js
+++ js/main.js
@@ -43,10 +43,9 @@ function (
 	          var vars = query.split('&');
 	          for (var i = 0; i < vars.length; i++) {
 		      var pair = vars[i].split('=');
-                      if (pair[0] == 'addBookmarks' || pair[0] == 'highlight') {
-                          next;
+                      if (!pair[0] == 'addBookmarks' && !pair[0] == 'highlight') {
+                          params.push(vars[i]);
                       }
-		      params.push(vars[i]);;
 	          }
                   var allparams = params.join(';');
                   console.log(allparams);

6432a83bc031ff20e8426208bf921782ac8dd611
Scott Cain <scott+git@scottcain.net>
2020-08-20 22:52:12 +0000

    just skipping the addBookmarks param

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ function (
 	          var vars = query.split('&');
 	          for (var i = 0; i < vars.length; i++) {
 		      var pair = vars[i].split('=');
-                      if (!(pair[0] == 'addBookmarks' || !pair[0] == 'highlight')) {
+                      if (!(pair[0] == 'addBookmarks')) {
                           params.push(vars[i]);
                       } 
 	          }
